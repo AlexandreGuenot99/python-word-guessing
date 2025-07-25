398b4c382adaa0ee1a3a622927b8a10a82c817f3
Alexandre Guenot <realnameherej@gmail.com>
2025-07-25 12:13:58 +0200

    word guessing complete

--- draft.py
+++ draft.py
@@ -1,4 +1,20 @@
-word = "hello"
-word_list = list(word)
-print(word_list)
-print(len(word))
+# elif user_character not in guessed_letter:
+#             guessed_letter.append(user_character)
+#             for i in range (0,len(word)):
+#                 if user_character == word[i]:
+#                     word_secret_list[i] = word[i]
+#                     word_secret = "".join(word_secret_list)
+elements = []
+while True : 
+    guess = input (">")
+    if guess in elements:
+        print("IN.")
+    if guess == 'q':
+        break
+    if guess not in elements:
+        elements.append(guess)
+        print("ADDED.")
+    print(elements)
+
+print(elements)
+
--- main.py
+++ main.py
@@ -4,11 +4,41 @@ list_of_words = ['rainbow', 'computer', 'science', 'programming',
          'reverse', 'water', 'board', 'geeks']
 
 word = list_of_words[random.randint(0,len(list_of_words)-1)]
-print("Guess the characters")
-chained_characters = 
+word_secret = '-'*len(word)
+word_secret_list = list('-'*len(word))
+print('Guess the characters')
+for characters in word_secret:
+    print(characters)
+
+guessed_letters = []
+wrong_letters =  []
+
 while True:
-    for i in range (0,len(word)):
-        print("-\n")
+    user_character = input("guess a character: ")   
+    user_character_alpha = user_character.isalpha()
+    if user_character_alpha: 
+        if user_character in word:
+            if user_character not in guessed_letters:
+                guessed_letters.append(user_character)
+                for i in range (0,len(word)):
+                    if user_character == word[i]:
+                        word_secret_list[i] = word[i]
+                        word_secret = "".join(word_secret_list)
+            else:
+                print("You already guessed this letter ")
+        elif user_character not in word: 
+            if user_character not in wrong_letters:
+                wrong_letters.append(user_character)
+            else:
+                print("You already tried this letter ")
+        if word_secret == word:
+            break
+    else :
+        print("You must type a letter")
+    for characters in word_secret:
+        print(characters)
+    print(f"\nRight letters guessed : {guessed_letters}\n")
+    print(f"Wrong letters tried : {wrong_letters}\n")
 
-
-
+print("You win")
+print(f"The word is:  {word}")
